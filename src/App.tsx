d0173b2b87062255898952b87b88f69e116ea7e3
Vova Boychyshyn <93547207+qsunn@users.noreply.github.com>
2022-08-10 12:13:43 +0300

    added hw

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,70 @@
+import { useState } from "react";
+import Contact from "./components/Contact";
+import { contacts } from "./data/contacts";
+
 function App() {
+  const [searchValue, setSearchValue] = useState("");
+  const [filterMale, setFilterMale] = useState(false);
+  const [filterFemale, setFilterFemale] = useState(false);
+  const [filterAnon, setFilterAnon] = useState(false);
+
   return (
-    <div className="min-h-[100vh] grid row-auto-fr place-items-center">
-      <h1 className="text-3xl font-bold">Hello World</h1>
+    <div className="min-h-[100vh] bg-slate-900 text-white grid auto-rows-min gap-[16px] p-[32px]">
+      <form className="w-[312px] text-black mx-auto">
+        <div className="flex justify-between text-white">
+          <span>
+            <input
+              type="checkbox"
+              id="male"
+              defaultChecked
+              className="mr-[8px]"
+              onClick={() => setFilterMale((prev) => !prev)}
+            />
+            <label htmlFor="male">Male</label>
+          </span>
+          <span>
+            <input
+              type="checkbox"
+              id="female"
+              defaultChecked
+              className="mr-[8px]"
+              onClick={() => setFilterFemale((prev) => !prev)}
+            />
+            <label htmlFor="female">Female</label>
+          </span>
+          <span>
+            <input
+              type="checkbox"
+              id="anon"
+              defaultChecked
+              className="mr-[8px]"
+              onClick={() => setFilterAnon((prev) => !prev)}
+            />
+            <label htmlFor="anon">Anon</label>
+          </span>
+        </div>
+
+        <input
+          type="text"
+          className="w-[100%] py-[8px] px-[16px] outline-none rounded-[4px] mt-[8px]"
+          placeholder="Search..."
+          value={searchValue}
+          onChange={(event) => setSearchValue(event.target.value.toLowerCase())}
+        />
+      </form>
+      {contacts
+        .filter(contact => filterMale ? contact.gender !== "male" : contact.gender !== 'all')
+        .filter(contact => filterFemale ? contact.gender !== "female" : contact.gender !== 'all')
+        .filter(contact => filterAnon ? contact.gender : contact.gender !== 'all')
+        .filter(
+          (contact) =>
+            contact.firstName.toLowerCase().includes(searchValue) ||
+            contact.lastName.toLowerCase().includes(searchValue) ||
+            contact.phone.toLowerCase().includes(searchValue)
+        )
+        .map((contact, i) => (
+          <Contact contact={contact} key={i} />
+        ))}
     </div>
   );
 }
